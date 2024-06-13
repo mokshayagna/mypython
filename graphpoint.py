cc8f00112fa871ed7dc83d116f7b64fc3ffdc546
Moksha Yagna <mokshayagna09@gmail.com>
2024-06-13 20:23:17 +0530

    adding files

--- graphpoint.py
+++ graphpoint.py
@@ -15,47 +15,35 @@ def dump_data_from_csv_file(filename):
 
 
 def calculating_distance(points):
-    distances = {}
+    distances = []
     for i in range(0,len(points)):
         x1, y1 = points[i]
-        #print(x1,y1)
+        print(x1,y1)
         print('') 
         for j in range(i+1, len(points)):  
             x2, y2 = points[j]
            # print(x2 ,y2)
             distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-            distances[((x1, y1), (x2, y2))] = distance
-           # print(((x1,y1),(x2,y2)), distance)
+            distances.append((distance,points[i]))
+            print(((x1,y1),(x2,y2)), distance)
     return(distances)
 
 def find_nearest_points(distances):
-    sorted_distances = sorted(distances.items(), key=lambda item: item[1])
-    nearest_points = sorted_distances[0][0] 
+   # print("distances", distances)
+    distances.sort()
+    # print("sorted", distances)
+    if (distances[0][0] == 0):
+        distances.pop(0)
+   # print(distances)
+    nearest_points = [distances[0][1],distances[1][1]]
+    print(nearest_points)
     return nearest_points
 
-def distance_between_points(distances, point1, point2):
-    if (point1, point2) in distances:
-        return distances[(point1, point2)]
-    elif (point2, point1) in distances:
-        return distances[(point2, point1)]
-    else:
-        return None 
-    
                 
 def main():
     points = dump_data_from_csv_file("graphpoints.csv")
     distances = calculating_distance(points)
-    nearest_points = find_nearest_points(distances)
-    print("The nearest points are:", nearest_points)
-
-
-    point1 = (1, 2)
-    point2 = (4, 6)
-    distance = distance_between_points(distances, point1, point2)
-    if distance is not None:
-        print(f"The distance between {point1} and {point2} is: {distance}")
-    else:
-        print(f"No distance found between {point1} and {point2}") 
+    find_nearest_points(distances) 
 
 if(__name__ == "__main__"):
     main()
