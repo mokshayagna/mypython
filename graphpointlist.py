f52cf43af21051460aac63c1c3beee26a71d107f
Moksha Yagna <mokshayagna09@gmail.com>
2024-06-13 20:20:03 +0530

    adding files

---
+++ graphpointlist.py
@@ -0,0 +1,49 @@
+import math
+import csv
+
+
+def dump_data_from_csv_file(filename):
+    data_list = []
+    with open(filename, 'rt') as fd:
+        fdata = csv.reader(fd)
+        next(fdata)
+        for i in fdata:
+            points = (int(i[0]), int(i[1]))
+            data_list.append(points)
+    return (data_list)
+            #print(point)
+
+
+def calculating_distance(points):
+    distances = []
+    for i in range(0,len(points)):
+        x1, y1 = points[i]
+        print(x1,y1)
+        print('') 
+        for j in range(i+1, len(points)):  
+            x2, y2 = points[j]
+           # print(x2 ,y2)
+            distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+            distances.append((distance,points[i]))
+            print(((x1,y1),(x2,y2)), distance)
+    return(distances)
+
+def find_nearest_points(distances):
+   # print("distances", distances)
+    distances.sort()
+    # print("sorted", distances)
+    if (distances[0][0] == 0):
+        distances.pop(0)
+   # print(distances)
+    nearest_points = [distances[0][1],distances[1][1]]
+    print(nearest_points)
+    return nearest_points
+
+                
+def main():
+    points = dump_data_from_csv_file("graphpoints.csv")
+    distances = calculating_distance(points)
+    find_nearest_points(distances) 
+
+if(__name__ == "__main__"):
+    main()
---
+++ nearestfruits.py
@@ -0,0 +1,33 @@
+import csv
+
+def dump_data_from_csv(filename):
+    data_dict = {}
+    with open(filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)  
+        for row in csvreader:
+            if len(row) == 3:  
+                fruit_name = row[0]
+                colour = int(row[1])
+                sweet = int(row[2])
+                data_dict[fruit_name] = (colour, sweet)
+            
+    return data_dict
+
+def nearest_fruit(fruits_dict, given_fruit):
+    if given_fruit in fruits_dict:
+        print(f"Nearest fruit to {given_fruit}: Colour={fruits_dict[given_fruit][0]}, Sweet={fruits_dict[given_fruit][1]}")
+    
+
+def main():
+    filename = 'fruits.csv'
+    fruits_dict = dump_data_from_csv(filename)
+    given_fruit = 'mango'
+    
+    for fruit, ratings in fruits_dict.items():
+        print(f"Fruit: {fruit}, Colour: {ratings[0]}, Sweet: {ratings[1]}")
+    
+    nearest_fruit(fruits_dict, given_fruit)
+
+if __name__ == "__main__":
+    main()
